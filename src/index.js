8542fc90e58e35ed6b3b33c93ab2bb0e51e32e56
Anthony Cox <anthonyjohnc@gmail.com>
2021-11-22 18:05:49 +0000

    add small right arrow icon component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 /* Export all of the header components */
 export { default as Header } from './ui-elements/header/Header';
+
+/* Export all of the icon components */
+export { default as SmallRightArrowIcon } from './ui-elements/icons/SmallRightArrowIcon';
