bc1b2a2d92cc47b8a9ba41aebd57c54fddf87101
Anthony Cox <anthonyjohnc@gmail.com>
2021-11-23 08:39:05 +0000

    add story for the document editor component

---
+++ stories/editor/DocumentEditor.stories.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { DocumentEditor } from '../../src';
+
+export default {
+	component: DocumentEditor,
+	title: 'Editing/Document Editor',
+};
+
+// const draftEditorItemsParagraphNoContent = [{"characterLimit":1000,"index":0,"isDeleted":false,"itemNumber":1,"itemType":"paragraph","textContent":""}];
+// const draftEditorItemsParagraphHeadingNoContent = [{"additionalUpperSpacing":true,"index":0,"isDeleted":false,"itemNumber":1,"itemType":"paragraph-heading","textContent":""}];
+// const draftEditorItemsScreenshotWithCaptionNoContent = [{"caption":"","characterLimit":50,"index":0,"isDeleted":false,"itemNumber":1,"itemType":"screenshot"}];
+
+const Template = args => {
+	return <div role="region">
+    <DocumentEditor {...args} />
+  </div>
+}
+
+export const Default = Template.bind({});
+Default.args = {};
+
+// export const RedEditor = Template.bind({});
+// RedEditor.args = {
+// 	colour: 'red',
+// 	dialogBgColour: 'grey',
+// };
+
+// export const PopulatedWithPreviousDraftDocument = Template.bind({});
+// PopulatedWithPreviousDraftDocument.args = {
+// 	colour: 'grey',
+// 	dialogBgColour: 'yellow',
+// 	previousDraftEditorItems: draftEditorItemsScreenshotWithCaptionNoContent,
+// };
