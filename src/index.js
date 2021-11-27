f1ad8d219686e9db7b40874ab0150240ae82d864
Anthony Cox <anthonyjohnc@gmail.com>
2021-11-27 19:07:08 +0000

    add dropdown menu component

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ export { default as DocumentEditor } from './ui-elements/editor/DocumentEditor';
 export { default as SmallFooter } from './ui-elements/footer/SmallFooter';
 
 /* Export all of the form controls components */
+export { default as DropdownMenu } from './ui-elements/form/DropdownMenu';
 export { default as HorizontalButtonList } from './ui-elements/form/HorizontalButtonList';
 
 /* Export all of the header components */
---
+++ src/ui-elements/form/DropdownMenu.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FormItemLabel from './supporting-elements/FormItemLabel';
+import '../../css/common.css';
+import '../../css/form-item.css';
+
+/**
+ * The DropdownMenu component allows you to render a dropdown menu either as a standalone or inside a dialog.
+ */
+class DropdownMenu extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const rootStyling = 'ajc-flex-column ajc-flex-justify-left ajc-background-transparent ajc-width-dialog-inner ajc-spacing-bottom-small ajc-text-black ajc-font-default';
+    const selectStyling = 'ajc-select ajc-font-default';
+    return (
+      <div className={rootStyling}>
+        <FormItemLabel id={this.props.id} labelText={this.props.labelText} additionalUpperSpacing={this.props.additionalUpperSpacing}
+          isDialogFormItem={this.props.isDialogFormItem} />
+        <select id={`${this.props.id}-dropdown-menu`} name={`${this.props.id}-dropdown-menu`} className={selectStyling} defaultValue={this.props.optionsList[0].value}>
+          {
+            this.props.optionsList.map((option, index) => {
+              return <option key={`${option.value}-${index}`} value={option.value} title={option.textContent}>{option.textContent}</option>
+            })
+          }
+        </select>
+      </div>
+    );
+  }
+}
+DropdownMenu.propTypes = {
+  /** The components unique identifier. */
+  id: PropTypes.string.isRequired,
+  /** The label text content for the component. */
+  labelText: PropTypes.string.isRequired,
+  /** The list of option items to be rendered in the dropdown menu. */
+  optionsList: PropTypes.arrayOf(PropTypes.shape({
+    textContent: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+  })).isRequired,
+  /** Switch for adding more spacing above the label. Use this to separate the label from other items above it. No additional spacing is added by default. */
+  additionalUpperSpacing: PropTypes.bool,
+  /** Switch for stating whether this form item appears in a dialog or not. By default this is set to false. */
+  isDialogFormItem: PropTypes.bool,
+};
+DropdownMenu.defaultProps = {
+  additionalUpperSpacing: false,
+  isDialogFormItem: false,
+};
+export default DropdownMenu;
