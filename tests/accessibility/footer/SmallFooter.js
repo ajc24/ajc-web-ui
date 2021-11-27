637149b3cda20322e07da85d8ad4bd670b4c60e4
Anthony Cox <anthonyjohnc@gmail.com>
2021-11-27 22:09:13 +0000

    updated to use accessibility checker

--- tests/accessibility/footer/SmallFooter.js
+++ tests/accessibility/footer/SmallFooter.js
@@ -1,52 +1,54 @@
 import 'jsdom-global/register';
 import React from 'react';
-import { toHaveNoViolations } from 'jest-axe';
 import { AccessibilityDev } from 'ajc-accessibility';
 import { TestDev } from 'ajc-jest-enzyme';
-import { SmallFooter } from '../../../src';
+import { PageContent, SmallFooter } from '../../../src';
 
 describe('SmallFooter', () => {
   /* Create the test data for the menu */
   const testTimeout = AccessibilityDev.getMaximumTimeout();
 
-  /* Extend the expect behaviour of jest */
-  expect.extend(toHaveNoViolations);
-
   describe('Default props and rendering - Component with grey background', () => {
-    let jestAxeResults;
+    let aCheckerResults
 
     beforeAll(async () => {
       const html = TestDev.mountHtmlTemplate(
-        <React.Fragment>
+        <div>
+          <PageContent title="Accessibility Test">
+            <p>Hello World</p>
+          </PageContent>
           <SmallFooter>
             This is a test.
           </SmallFooter>
-        </React.Fragment>
+        </div>
       );
-      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+      aCheckerResults = await AccessibilityDev.runAccessibilityChecker(html, 'Default small footer');
     }, testTimeout);
 
-    it('verifies the jest-axe accessibility standards for the component', () => {
-      expect(jestAxeResults).toHaveNoViolations();
+    it('verifies the accessibility checker standards for the component', () => {
+      expect(aCheckerResults).toBeTruthy();
     });
   });
 
   describe('Transferred props and rendering - Component with red background', () => {
-    let jestAxeResults;
+    let aCheckerResults
 
     beforeAll(async () => {
       const html = TestDev.mountHtmlTemplate(
-        <React.Fragment>
+        <div>
+          <PageContent title="Accessibility Test">
+            <p>Hello World</p>
+          </PageContent>
           <SmallFooter colour="red">
             This is a test.
           </SmallFooter>
-        </React.Fragment>
+        </div>
       );
-      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+      aCheckerResults = await AccessibilityDev.runAccessibilityChecker(html, 'Small footer with red background');
     }, testTimeout);
 
-    it('verifies the jest-axe accessibility standards for the component', () => {
-      expect(jestAxeResults).toHaveNoViolations();
+    it('verifies the accessibility checker standards for the component', () => {
+      expect(aCheckerResults).toBeTruthy();
     });
   });
 });
