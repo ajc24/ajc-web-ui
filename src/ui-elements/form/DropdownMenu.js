979615aad17842ddd721f5b1441beff32aec8528
Anthony Cox <anthonyjohnc@gmail.com>
2021-11-30 17:47:54 +0000

    update accessibility standards of dropdown menu component

--- src/ui-elements/form/DropdownMenu.js
+++ src/ui-elements/form/DropdownMenu.js
@@ -13,13 +13,13 @@ class DropdownMenu extends React.Component {
   }
 
   render() {
-    const rootStyling = 'ajc-flex-column ajc-flex-justify-left ajc-background-transparent ajc-width-dialog-inner ajc-spacing-bottom-small ajc-text-black ajc-font-default';
+    const rootStyling = 'ajc-flex-column ajc-flex-justify-left ajc-background-transparent ajc-spacing-bottom-small ajc-text-black ajc-font-default';
     const selectStyling = 'ajc-select ajc-font-default';
     return (
       <div className={rootStyling}>
         <FormItemLabel id={this.props.id} labelText={this.props.labelText} additionalUpperSpacing={this.props.additionalUpperSpacing}
           isDialogFormItem={this.props.isDialogFormItem} />
-        <select id={`${this.props.id}-dropdown-menu`} name={`${this.props.id}-dropdown-menu`} className={selectStyling} defaultValue={this.props.optionsList[0].value}>
+        <select id={this.props.id} name={this.props.id} className={selectStyling} defaultValue={this.props.optionsList[0].value}>
           {
             this.props.optionsList.map((option, index) => {
               return <option key={`${option.value}-${index}`} value={option.value} title={option.textContent}>{option.textContent}</option>
