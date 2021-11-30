078ed9f609e93d6bbc8a16e6fc5d556727260a25
Anthony Cox <anthonyjohnc@gmail.com>
2021-11-30 13:27:15 +0000

    updated to use axe-core for accessibility checks

--- tests/accessibility/page-mask/PageMask.js
+++ tests/accessibility/page-mask/PageMask.js
@@ -1,90 +1,90 @@
 import 'jsdom-global/register';
 import React from 'react';
-import { toHaveNoViolations } from 'jest-axe';
 import { AccessibilityDev } from 'ajc-accessibility';
 import { TestDev } from 'ajc-jest-enzyme';
-import { PageMask } from '../../../src';
+import { PageContent, PageMask } from '../../../src';
 
 describe('PageMask', () => {
   /* Create the test data for the menu */
   const testTimeout = AccessibilityDev.getMaximumTimeout();
 
-  /* Extend the expect behaviour of jest */
-  expect.extend(toHaveNoViolations);
-
   describe('Default props and rendering - Non visible page mask', () => {
-    let jestAxeResults;
+    let results;
 
     beforeAll(async () => {
       const html = TestDev.mountHtmlTemplate(
-        <div role="region">
+        <PageContent title="Accessibility Test">
+          <h1>Page Mask Accessibility Test</h1>
           <PageMask id="test-id" onClose={() => {}}>
             Test Page Mask text.
           </PageMask>
-        </div>
+        </PageContent>
       );
-      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+      results = await AccessibilityDev.runAxeCore(html);
     }, testTimeout);
 
-    it('verifies the jest-axe accessibility standards for the component', () => {
-      expect(jestAxeResults).toHaveNoViolations();
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
     });
   });
 
   describe('Transferred props and rendering - Component with grey theme', () => {
-    let jestAxeResults;
+    let results;
 
     beforeAll(async () => {
       const html = TestDev.mountHtmlTemplate(
-        <div role="region">
+        <PageContent title="Accessibility Test">
+          <h1>Page Mask Accessibility Test</h1>
           <PageMask id="test-id" isDisplayed={true} colour="grey" onClose={() => {}}>
             Test Page Mask text.
           </PageMask>
-        </div>
+        </PageContent>
       );
-      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+      results = await AccessibilityDev.runAxeCore(html);
     }, testTimeout);
 
-    it('verifies the jest-axe accessibility standards for the component', () => {
-      expect(jestAxeResults).toHaveNoViolations();
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
     });
   });
 
   describe('Transferred props and rendering - Component with red theme', () => {
-    let jestAxeResults;
+    let results;
 
     beforeAll(async () => {
       const html = TestDev.mountHtmlTemplate(
-        <div role="region">
+        <PageContent title="Accessibility Test">
+          <h1>Page Mask Accessibility Test</h1>
           <PageMask id="test-id" isDisplayed={true} colour="red" onClose={() => {}}>
             Test Page Mask text.
           </PageMask>
-        </div>
+        </PageContent>
       );
-      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+      results = await AccessibilityDev.runAxeCore(html);
     }, testTimeout);
 
-    it('verifies the jest-axe accessibility standards for the component', () => {
-      expect(jestAxeResults).toHaveNoViolations();
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
     });
   });
 
   describe('Transferred props and rendering - onClose functionality not set', () => {
-    let jestAxeResults;
+    let results;
 
     beforeAll(async () => {
       const html = TestDev.mountHtmlTemplate(
-        <div role="region">
+        <PageContent title="Accessibility Test">
+          <h1>Page Mask Accessibility Test</h1>
           <PageMask id="test-id" isDisplayed={true}>
             Test Page Mask text.
           </PageMask>
-        </div>
+        </PageContent>
       );
-      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+      results = await AccessibilityDev.runAxeCore(html);
     }, testTimeout);
 
-    it('verifies the jest-axe accessibility standards for the component', () => {
-      expect(jestAxeResults).toHaveNoViolations();
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
     });
   });
 });
--- tests/accessibility/page-mask/PageMaskCloseButton.js
+++ tests/accessibility/page-mask/PageMaskCloseButton.js
@@ -1,65 +1,64 @@
 import 'jsdom-global/register';
 import React from 'react';
-import { toHaveNoViolations } from 'jest-axe';
 import { AccessibilityDev } from 'ajc-accessibility';
 import { TestDev } from 'ajc-jest-enzyme';
-import { PageMaskCloseButton } from '../../../src';
+import { PageContent, PageMaskCloseButton } from '../../../src';
 
 describe('PageMaskCloseButton', () => {
   /* Create the test data for the menu */
   const testTimeout = AccessibilityDev.getMaximumTimeout();
 
-  /* Extend the expect behaviour of jest */
-  expect.extend(toHaveNoViolations);
-
   describe('Default props and rendering - onClose functionality not set', () => {
-    let jestAxeResults;
+    let results;
 
     beforeAll(async () => {
       const html = TestDev.mountHtmlTemplate(
-        <div role="region">
+        <PageContent title="Accessibility Test">
+          <h1>Page Mask Close Button Accessibility Test</h1>
           <PageMaskCloseButton id="test-id" />
-        </div>
+        </PageContent>
       );
-      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+      results = await AccessibilityDev.runAxeCore(html);
     }, testTimeout);
 
-    it('verifies the jest-axe accessibility standards for the component', () => {
-      expect(jestAxeResults).toHaveNoViolations();
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
     });
   });
 
   describe('Transferred props and rendering - Default component with onClose functionality set', () => {
-    let jestAxeResults;
+    let results;
 
     beforeAll(async () => {
       const html = TestDev.mountHtmlTemplate(
-        <div role="region">
+        <PageContent title="Accessibility Test">
+          <h1>Page Mask Close Button Accessibility Test</h1>
           <PageMaskCloseButton id="test-id" onClose={() => {}} />
-        </div>
+        </PageContent>
       );
-      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+      results = await AccessibilityDev.runAxeCore(html);
     }, testTimeout);
 
-    it('verifies the jest-axe accessibility standards for the component', () => {
-      expect(jestAxeResults).toHaveNoViolations();
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
     });
   });
 
   describe('Transferred props and rendering - Red button with onClose functionality set', () => {
-    let jestAxeResults;
+    let results;
 
     beforeAll(async () => {
       const html = TestDev.mountHtmlTemplate(
-        <div role="region">
+        <PageContent title="Accessibility Test">
+          <h1>Page Mask Close Button Accessibility Test</h1>
           <PageMaskCloseButton id="test-id" colour="red" onClose={() => {}} />
-        </div>
+        </PageContent>
       );
-      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+      results = await AccessibilityDev.runAxeCore(html);
     }, testTimeout);
 
-    it('verifies the jest-axe accessibility standards for the component', () => {
-      expect(jestAxeResults).toHaveNoViolations();
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
     });
   });
 });
