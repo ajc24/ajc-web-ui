1a5ba53b2fc902a1b518bec99ae70e204c5978e0
Anthony Cox <anthonyjohnc@gmail.com>
2022-01-25 19:11:04 +0000

    add support for the new upper controls element

--- src/ui-elements/editor/supporting-elements/controls/ControlsEditor.js
+++ src/ui-elements/editor/supporting-elements/controls/ControlsEditor.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import HorizontalButtonList from '../../../form/HorizontalButtonList';
+import ControlsEditorUpper from './supporting-elements/ControlsEditorUpper';
 
 /**
  * The ControlsEditor component renders the controls / buttons for the editor panel of the document editor.
@@ -12,82 +12,66 @@ class ControlsEditor extends React.Component {
 
   render() {
     /* Set the buttons list for the editor controls */
-    const upperEditorButtonsList = [
-      {
-        id: 'add-new-item-button',
-        onClick: this.props.handleClickAddNewItem,
-        title: 'Add New Item',
-        tooltip: 'Add a new item to your document.',
-        type: 'button',
-      },
-    ];
-    const middleEditorButtonsList = [
-      {
-        id: 'save-as-draft-button',
-        onClick: () => { alert('Clicked save as draft button.'); },
-        title: 'Save as Draft',
-        tooltip: 'Save your document in its current incomplete state. You may return to finish it later.',
-        type: 'button',
-      },
-      {
-        id: 'generate-preview-button',
-        onClick: () => { alert('Clicked generate preview button.'); },
-        title: 'Generate Preview',
-        tooltip: 'Generate a preview of your document in its current state.',
-        type: 'button',
-      },
-      {
-        id: 'publish-document-button',
-        onClick: () => { alert('Clicked publish document button.'); },
-        title: 'Publish Document',
-        tooltip: 'When you are finished with your document, click here to publish it.',
-        type: 'button',
-      },
-    ];
-    const lowerEditorButtonsList = [
-      {
-        id: 'discard-draft-button',
-        onClick: () => { alert('Clicked discard draft button.'); },
-        title: 'Discard Draft',
-        tooltip: 'Discard your current working document. Please note you will not be able to return to it after discarding it.',
-        type: 'button',
-      }
-    ];
+    // const middleEditorButtonsList = [
+    //   {
+    //     id: 'save-as-draft-button',
+    //     onClick: () => { alert('Clicked save as draft button.'); },
+    //     title: 'Save as Draft',
+    //     tooltip: 'Save your document in its current incomplete state. You may return to finish it later.',
+    //     type: 'button',
+    //   },
+    //   {
+    //     id: 'generate-preview-button',
+    //     onClick: () => { alert('Clicked generate preview button.'); },
+    //     title: 'Generate Preview',
+    //     tooltip: 'Generate a preview of your document in its current state.',
+    //     type: 'button',
+    //   },
+    //   {
+    //     id: 'publish-document-button',
+    //     onClick: () => { alert('Clicked publish document button.'); },
+    //     title: 'Publish Document',
+    //     tooltip: 'When you are finished with your document, click here to publish it.',
+    //     type: 'button',
+    //   },
+    // ];
+    // const lowerEditorButtonsList = [
+    //   {
+    //     id: 'discard-draft-button',
+    //     onClick: () => { alert('Clicked discard draft button.'); },
+    //     title: 'Discard Draft',
+    //     tooltip: 'Discard your current working document. Please note you will not be able to return to it after discarding it.',
+    //     type: 'button',
+    //   }
+    // ];
     return (
       <React.Fragment>
-        <HorizontalButtonList alignment="left" additionalUpperSpacing={this.props.upperButtonListAdditionalSpacing} colour={this.props.colour}
-          buttonList={upperEditorButtonsList} labelText="Click the button below to add new items to your document:" />
-        <HorizontalButtonList alignment="left" additionalUpperSpacing={true} colour={this.props.colour} buttonList={middleEditorButtonsList}
+        <ControlsEditorUpper additionalSpacingAboveControls={this.props.additionalSpacingAboveControls} colour={this.props.colour}
+          addEditorItem={this.props.addEditorItem} dialogContentAreaColour={this.props.dialogContentAreaColour} reRenderAllowance={this.props.reRenderAllowance} />
+        {/* <HorizontalButtonList alignment="left" additionalUpperSpacing={true} colour={this.props.colour} buttonList={middleEditorButtonsList}
           labelText="Save your current draft, generate a preview or publish your document:" />
         <HorizontalButtonList alignment="left" additionalUpperSpacing={true} colour={this.props.colour} buttonList={lowerEditorButtonsList}
-          labelText="Click the button below to discard this current draft:" />
+          labelText="Click the button below to discard this current draft:" /> */}
       </React.Fragment>
     );
   }
 }
 ControlsEditor.propTypes = {
+  /** The functionality to be carried out to add a new item to the editor. */
+  addEditorItem: PropTypes.func.isRequired,
   /** The colour / theme set to the document editor buttons and all relevant items in the preview page. */
   colour: PropTypes.oneOf([ 'grey', 'red' ]),
-  /** The click functionality for the add new item button. */
-  handleClickAddNewItem: PropTypes.func,
-  /** The click functionality for the discard draft button. */
-  // onClickDiscardDraft: PropTypes.func,
-  // /** The click functionality for the generate preview button. */
-  // onClickGeneratePreview: PropTypes.func,
-  // /** The click functionality for the publish document button. */
-  // onClickPublishDocument: PropTypes.func,
-  // /** The click functionality for the save as draft button. */
-  // onClickSaveAsDraft: PropTypes.func,
-  /** Use additional spacing above the upper editor button list. */
-  upperButtonListAdditionalSpacing: PropTypes.bool,
+  /** The background colour for the content area of any dialogs rendered. By default this is set to white. */
+  dialogContentAreaColour: PropTypes.oneOf([ 'grey', 'white', 'yellow' ]),
+  /** The time allowance between a state change and a re-render occurring. */
+  reRenderAllowance: PropTypes.number,
+  /** Use additional spacing above the upper buttons. */
+  additionalSpacingAboveControls: PropTypes.bool,
 };
 ControlsEditor.defaultProps = {
   colour: 'grey',
-  handleClickAddNewItem: () => { alert('Clicked add new item button.'); },
-  // onClickDiscardDraft: () => {},
-  // onClickGeneratePreview: () => {},
-  // onClickPublishDocument: () => {},
-  // onClickSaveAsDraft: () => {},
-  upperButtonListAdditionalSpacing: false,
+  dialogContentAreaColour: 'white',
+  reRenderAllowance: 100,
+  additionalSpacingAboveControls: false,
 };
 export default ControlsEditor;
