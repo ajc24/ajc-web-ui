d876d583138a808ad0d5f98fe00257cc54419814
Anthony Cox <anthonyjohnc@gmail.com>
2022-01-25 19:11:21 +0000

    update unit test to support all component changes

--- tests/unit/editor/supporting-elements/controls/ControlsEditor.js
+++ tests/unit/editor/supporting-elements/controls/ControlsEditor.js
@@ -5,120 +5,35 @@ import ControlsEditor from '../../../../../src/ui-elements/editor/supporting-ele
 
 describe('ControlsEditor', () => {
   describe('Default props and rendering', () => {
-    let alertDefaultFunction;
-    let windowAlertSpy;
     let wrapper;
 
     beforeAll(() => {
-      /* Set up the spy for the alert */
-      alertDefaultFunction = global.alert;
-      windowAlertSpy = jest.fn();
-      global.alert = windowAlertSpy;
       /* Mount the component */
       wrapper = TestDev.mount(
         <React.Fragment>
-          <ControlsEditor />
+          <ControlsEditor addEditorItem={jest.fn()} />
         </React.Fragment>
       );
     });
 
-    afterAll(() => {
-      global.alert = alertDefaultFunction;
+    it('verifies that the additionalSpacingAboveControls property is correctly passed to the upper editor controls component', () => {
+      expect(wrapper.find('ControlsEditorUpper').prop('additionalSpacingAboveControls')).toBeFalsy();
     });
 
-    it('verifies that the alignment property is correctly passed to the first horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(0).prop('alignment')).toBe('left');
+    it('verifies that the colour property is correctly passed to the upper editor controls component', () => {
+      expect(wrapper.find('ControlsEditorUpper').prop('colour')).toBe('grey');
     });
 
-    it('verifies that the additionalUpperSpacing property is correctly passed to the first horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(0).prop('additionalUpperSpacing')).toBeFalsy();
+    it('verifies that the addEditorItem property is correctly passed to the upper editor controls component', () => {
+      expect(wrapper.find('ControlsEditorUpper').prop('addEditorItem')).toBeDefined();
     });
 
-    it('verifies that the colour property is correctly passed to the first horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(0).prop('colour')).toBe('grey');
+    it('verifies that the dialogContentAreaColour property is correctly passed to the upper editor controls component', () => {
+      expect(wrapper.find('ControlsEditorUpper').prop('dialogContentAreaColour')).toBe('white');
     });
 
-    it('verifies that the buttonList property is correctly passed to the first horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(0).prop('buttonList')).toHaveLength(1);
-    });
-
-    it('verfies that the default onClick functionality is invoked when the add new item button is clicked', () => {
-      wrapper.find('button#add-new-item-button').prop('onClick')();
-      const numberOfAlerts = windowAlertSpy.mock.calls.length;
-      windowAlertSpy.mockClear();
-      expect(numberOfAlerts).toBe(1);
-    });
-
-    it('verifies that the labelText property is correctly passed to the first horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(0).prop('labelText')).toBe('Click the button below to add new items to your document:');
-    });
-
-    it('verifies that the alignment property is correctly passed to the second horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(1).prop('alignment')).toBe('left');
-    });
-
-    it('verifies that the additionalUpperSpacing property is correctly passed to the second horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(1).prop('additionalUpperSpacing')).toBeTruthy();
-    });
-
-    it('verifies that the colour property is correctly passed to the second horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(1).prop('colour')).toBe('grey');
-    });
-
-    it('verifies that the buttonList property is correctly passed to the second horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(1).prop('buttonList')).toHaveLength(3);
-    });
-
-    it('verfies that the default onClick functionality is invoked when the save as draft button is clicked', () => {
-      wrapper.find('button#save-as-draft-button').prop('onClick')();
-      const numberOfAlerts = windowAlertSpy.mock.calls.length;
-      windowAlertSpy.mockClear();
-      expect(numberOfAlerts).toBe(1);
-    });
-
-    it('verfies that the default onClick functionality is invoked when the generate preview button is clicked', () => {
-      wrapper.find('button#generate-preview-button').prop('onClick')();
-      const numberOfAlerts = windowAlertSpy.mock.calls.length;
-      windowAlertSpy.mockClear();
-      expect(numberOfAlerts).toBe(1);
-    });
-
-    it('verfies that the default onClick functionality is invoked when the publish document button is clicked', () => {
-      wrapper.find('button#publish-document-button').prop('onClick')();
-      const numberOfAlerts = windowAlertSpy.mock.calls.length;
-      windowAlertSpy.mockClear();
-      expect(numberOfAlerts).toBe(1);
-    });
-
-    it('verifies that the labelText property is correctly passed to the second horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(1).prop('labelText')).toBe('Save your current draft, generate a preview or publish your document:');
-    });
-
-    it('verifies that the alignment property is correctly passed to the third horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(2).prop('alignment')).toBe('left');
-    });
-
-    it('verifies that the additionalUpperSpacing property is correctly passed to the third horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(2).prop('additionalUpperSpacing')).toBeTruthy();
-    });
-
-    it('verifies that the colour property is correctly passed to the third horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(2).prop('colour')).toBe('grey');
-    });
-
-    it('verifies that the buttonList property is correctly passed to the third horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(2).prop('buttonList')).toHaveLength(1);
-    });
-
-    it('verfies that the default onClick functionality is invoked when the discard draft button is clicked', () => {
-      wrapper.find('button#discard-draft-button').prop('onClick')();
-      const numberOfAlerts = windowAlertSpy.mock.calls.length;
-      windowAlertSpy.mockClear();
-      expect(numberOfAlerts).toBe(1);
-    });
-
-    it('verifies that the labelText property is correctly passed to the third horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(2).prop('labelText')).toBe('Click the button below to discard this current draft:');
+    it('verifies that the reRenderAllowance property is correctly passed to the upper editor controls component', () => {
+      expect(wrapper.find('ControlsEditorUpper').prop('reRenderAllowance')).toBeDefined();
     });
   });
 
@@ -130,50 +45,25 @@ describe('ControlsEditor', () => {
       handleClickAddNewItemMock = jest.fn();
       wrapper = TestDev.mount(
         <React.Fragment>
-          <ControlsEditor colour="red" handleClickAddNewItem={handleClickAddNewItemMock} upperButtonListAdditionalSpacing={true} />
+          <ControlsEditor colour="red" addEditorItem={jest.fn()} additionalSpacingAboveControls={true} dialogContentAreaColour="grey" reRenderAllowance={500} />
         </React.Fragment>
       );
     });
 
-    it('verifies that the additionalUpperSpacing property is correctly passed to the first horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(0).prop('additionalUpperSpacing')).toBeTruthy();
+    it('verifies that the additionalSpacingAboveControls property is correctly passed to the upper editor controls component', () => {
+      expect(wrapper.find('ControlsEditorUpper').prop('additionalSpacingAboveControls')).toBeTruthy();
     });
 
-    it('verifies that the colour property is correctly passed to the first horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(0).prop('colour')).toBe('red');
+    it('verifies that the colour property is correctly passed to the upper editor controls component', () => {
+      expect(wrapper.find('ControlsEditorUpper').prop('colour')).toBe('red');
     });
 
-    it('verfies that the custom onClick functionality is invoked when the add new item button is clicked', () => {
-      wrapper.find('button#add-new-item-button').prop('onClick')();
-      expect(handleClickAddNewItemMock.mock.calls).toHaveLength(1);
+    it('verifies that the dialogContentAreaColour property is correctly passed to the upper editor controls component', () => {
+      expect(wrapper.find('ControlsEditorUpper').prop('dialogContentAreaColour')).toBe('grey');
     });
 
-    it('verifies that the colour property is correctly passed to the second horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(1).prop('colour')).toBe('red');
+    it('verifies that the reRenderAllowance property is correctly passed to the upper editor controls component', () => {
+      expect(wrapper.find('ControlsEditorUpper').prop('reRenderAllowance')).toBe(500);
     });
-
-    // it('verfies that the custom onClick functionality is invoked when the save as draft button is clicked', () => {
-    //   wrapper.find('button#save-as-draft-button').prop('onClick')();
-    //   expect(handleClickAddNewItemMock.mock.calls).toHaveLength(1);
-    // });
-
-    // it('verfies that the custom onClick functionality is invoked when the generate preview button is clicked', () => {
-    //   wrapper.find('button#generate-preview-button').prop('onClick')();
-    //   expect(handleClickAddNewItemMock.mock.calls).toHaveLength(1);
-    // });
-
-    // it('verfies that the custom onClick functionality is invoked when the publish document button is clicked', () => {
-    //   wrapper.find('button#publish-document-button').prop('onClick')();
-    //   expect(handleClickAddNewItemMock.mock.calls).toHaveLength(1);
-    // });
-
-    it('verifies that the colour property is correctly passed to the third horizontal button list element', () => {
-      expect(wrapper.find('HorizontalButtonList').at(2).prop('colour')).toBe('red');
-    });
-
-    // it('verfies that the default onClick functionality is invoked when the discard draft button is clicked', () => {
-    //   wrapper.find('button#discard-draft-button').prop('onClick')();
-    //   expect(handleClickAddNewItemMock.mock.calls).toHaveLength(1);
-    // });
   });
 });
